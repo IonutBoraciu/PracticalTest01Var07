715eb6b051b1f41afdacc63322546a7100f215a5
IonutBoraciu <119059862+IonutBoraciu@users.noreply.github.com>
2025-11-12 08:25:50 +0200

    Solved first 2 exercices

--- app/src/main/java/com/example/practicaltest01var07/MainActivity.kt
+++ app/src/main/java/com/example/practicaltest01var07/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.practicaltest01var07
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,40 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toggleButton = ButtonClickListener()
+
+        val buttons = listOf(
+            R.id.random
+        )
+
+        for (id in buttons) {
+            findViewById<View>(id).setOnClickListener(toggleButton)
+        }
+    }
+
+    inner class ButtonClickListener : android.view.View.OnClickListener {
+        override fun onClick(v: View?) {
+            if (v == null) return
+
+            if(v.id == R.id.random) {
+                var left_1 = findViewById<EditText>(R.id.left_1)
+                var left_2 = findViewById<EditText>(R.id.left_2)
+                var right_1 = findViewById<EditText>(R.id.right_1)
+                var right_2 = findViewById<EditText>(R.id.right_2)
+                var random_num = (0..10).random()
+
+                if(left_1.text.toString() == "") {
+                    left_1.setText(random_num.toString())
+                } else if(right_1.text.toString() == "") {
+                    right_1.setText(random_num.toString())
+                } else if(left_2.text.toString() == "") {
+                    left_2.setText(random_num.toString())
+                } else if(right_2.text.toString() == "") {
+                    right_2.setText(random_num.toString())
+                }
+            }
         }
     }
 }
